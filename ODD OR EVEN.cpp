94c2a5a25a5e123a1abee2575e2aa9d8e5fdf584
keshri1989 <95084871+keshri1989@users.noreply.github.com>
2021-11-26 18:20:24 +0530

    Add files via upload

---
+++ ODD OR EVEN.cpp
@@ -0,0 +1,18 @@
+ #include<stdio.h>
+ int main()
+ {
+
+	int a;
+	printf("\n\n ODD OR EVEN \n\n");
+	printf("\n\nEnter the number :\n\n");
+	scanf("\n%d",&a);
+	if(a % 2 ==0)
+	{
+		printf("\n The given no. is Even");
+	}
+    else
+    {
+    	printf("the no is odd");
+	}
+	return 0;
+ }

fa79e484f61e8eee2d9520fb2b97a49a68d38bf0
keshri1989 <95084871+keshri1989@users.noreply.github.com>
2021-11-26 18:22:40 +0530

    Add files via upload

---
+++ HelloWorld.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+ 
+ int main()
+{  
+    char input[1000];
+    //scanf("%s",input);
+    gets(input);
+   printf("Hello, World\n");
+   //printf("%s",input);
+	puts(input);
+   return 0;
+
+}
+
+
+
+/*sample input:
+take one string input
+
+sample output:
+Hello, World
+the input you have taken
+*/
+
